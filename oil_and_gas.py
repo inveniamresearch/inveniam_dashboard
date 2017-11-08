bdd9d07a08c1740981df3901b1ac3678b09cc04b
chansoul <chansoul@adobe.com>
2017-11-09 02:03:38 +0530

    ini file addition

--- oil_and_gas.py
+++ oil_and_gas.py
@@ -1,7 +1,7 @@
 import dash_html_components as html
 
 
-layout = html.Iframe(src="http://127.0.0.1:8050/dash/gallery/new-york-oil-and-gas/",style={
+layout = html.Iframe(src="http://54.88.94.245:83/dash/gallery/new-york-oil-and-gas/",style={
     "frameborder":"0",
     "width":'100%',
     "height":"1500px",
